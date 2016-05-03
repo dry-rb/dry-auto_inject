7639bcd15870478edafeabf4fe11ef4b1b4f676a
Tim Riley <tim@icelab.com.au>
2016-05-03 21:40:11 +1000

    Make super method detection more robust (#10)
    Allow it to work when a class already has an initialize method defined, so the auto-injected dependencies can be passed straight to it.

---
+++ spec/integration/inheritance_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe "Inheritance" do
+  before do
+    module Test
+      AutoInject = Dry::AutoInject({one: 1, two: 2, 'namespace.three' => 3})
+    end
+  end
+
+  context "auto-inject included from outside a class with an existing initializer to manage ivar assignment" do
+    let(:class_with_initializer) do
+      Class.new do
+        attr_reader :one
+
+        def initialize(one)
+          @one = one
+        end
+      end
+    end
+
+    it "passes dependencies to the initializer" do
+      # This example only works on more modern Ruby version (2.1 and newer)
+      skip unless Class.instance_methods.include?(:include)
+
+      class_with_initializer.include Test::AutoInject[:one]
+      expect(class_with_initializer.new.one).to eq 1
+    end
+  end
+end
