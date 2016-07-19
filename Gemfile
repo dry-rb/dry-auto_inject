a950b8f65d3e70a1e98169d4c43a791fe49e7e22
Tim Riley <tim@openmonkey.com>
2016-07-19 15:43:42 +0800

    Add Code Climate test coverage reporting

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in dry-container.gemspec
 gemspec
 
+group :test do
+  gem "codeclimate-test-reporter", require: nil
+end
+
 group :tools do
   gem 'byebug', platforms: :mri
   gem 'pry'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,18 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'dry-auto_inject'
+if RUBY_ENGINE == "ruby"
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+
+  require "simplecov"
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+end
 
 begin
   require 'byebug'
 rescue LoadError; end
+
+require 'dry-auto_inject'
 
 module Test
   def self.remove_constants
