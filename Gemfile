02411a9598bf8abc2b3c2d12c45d738f21b7938f
Tim Riley <tim@openmonkey.com>
2016-12-14 08:09:28 +1100

    Fix coverage reporting on CI

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 gemspec
 
 group :test do
+  gem "simplecov"
   gem "codeclimate-test-reporter", require: nil
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
-if RUBY_ENGINE == "ruby"
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-
-  require "simplecov"
-  SimpleCov.start do
-    add_filter "/spec/"
-  end
+if RUBY_ENGINE == 'ruby' && RUBY_VERSION >= '2.3'
+  require 'simplecov'
+  SimpleCov.start
 end
 
 begin
