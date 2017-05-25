f7aad35ea3f38edf82ef2d7f3461284073f3bf2d
Nikita Shilnikov <fg@flashgordon.ru>
2017-05-25 23:18:42 +0300

    Update Travis setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
-if RUBY_ENGINE == 'ruby' && RUBY_VERSION >= '2.3'
-  require 'simplecov'
-  SimpleCov.start
+if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
+  require 'yaml'
+  rubies = YAML.load(File.read(File.join(__dir__, '..', '.travis.yml')))['rvm']
+  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
+
+  if RUBY_VERSION == latest_mri
+    require 'simplecov'
+    SimpleCov.start do
+      add_filter '/spec/'
+    end
+  end
 end
 
 begin
