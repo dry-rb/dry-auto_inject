a9471dbdc1b9ce8875a62400d38be8c675e4448b
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-08-20 03:38:20 +0300

    Adds support for custom namespace delimiters

--- lib/dry/auto_inject/dependency_map.rb
+++ lib/dry/auto_inject/dependency_map.rb
@@ -1,6 +1,8 @@
 module Dry
   module AutoInject
     DuplicateDependencyError = Class.new(StandardError)
+    DependencyNameIsNotValid = Class.new(StandardError)
+    VALID_NAME = /([a-z_][a-zA-Z_0-9]*)$/.freeze
 
     class DependencyMap
       def initialize(*dependencies)
@@ -10,7 +12,7 @@ module Dry
         aliases = dependencies.last.is_a?(Hash) ? dependencies.pop : {}
 
         dependencies.each do |identifier|
-          name = identifier.to_s.split(".").last
+          name = name_for(identifier)
           add_dependency(name, identifier)
         end
 
@@ -33,6 +35,12 @@ module Dry
       alias_method :to_hash, :to_h
 
       private
+
+      def name_for(identifier)
+        matched = VALID_NAME.match(identifier.to_s)
+        raise DependencyNameIsNotValid, "#{identifier} is not a valid Ruby identifier" unless matched
+        matched[0]
+      end
 
       def add_dependency(name, identifier)
         name = name.to_sym
